59fa9ed326a2a333c24c035c1383750159404c50
adakri <71411281+adakri@users.noreply.github.com>
2021-03-27 20:55:54 +0100

    Add files via upload

--- BC.cpp
+++ BC.cpp
@@ -1,1 +1,44 @@
-.
+#ifndef _BC_CPP
+
+#include "BC.h"
+#include <cmath>
+
+BC::BC() 
+{
+  std::cout << "La lecture des fonctions relatives au problème" << std::endl;
+  std::cout << "-------------------------------------------------" << std::endl;
+}
+
+
+double BC::Initial_condition(const double x, const double y) const
+{
+  return 0.; 
+}
+
+
+double BC::Source_term(const double x, const double y, const double t) const
+{
+  return 0.;
+  
+}
+
+double BC::Neumann_Function(const double x, const double y, const double t) const
+{
+  return 0.;
+ 
+}
+
+double BC::Dirichlet_Function(const double x, const double y, const double t) const
+{
+  return 0.;
+ 
+}
+
+double BC::Exact_solution(const double x, const double y, const double t) const
+{
+  return 0.;
+}
+
+
+#define _BC_CPP
+#endif
---
+++ Problem.h
@@ -0,0 +1,30 @@
+#ifndef PBDEF
+#define PBDEF
+
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "BC.h"
+
+class Problem
+{
+private:
+    int N_,Nt_;
+    double Lx_,Ly_,t;
+    const double D=1.;
+    std::vector<double> A_,F_;
+    BC* BC_functions_;
+
+public:
+    //constructor
+    Problem(BC* BC_functions);
+    //destructor
+    ~Problem();
+    std::vector<double> Construct_Matrix(int, double, double){return A_;};
+    std::vector<double> Construct_F(int, double, double){return F_;};
+};
+
+
+#endif
