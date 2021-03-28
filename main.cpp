5ac9721d4a34fb26059c61b04925d994ff132553
abdelmouttaleb <abdelmouttalebdakri@gmail.com>
2021-03-28 22:06:11 +0200

    add affichage

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+
+#include <math.h>
 #include "Problem.h"
 
 using namespace std;
+
+
+void print_matrix(std::vector<double> A)
+{
+  int n=sqrt(A.size());
+  cout<<"la matrice de taille "<<n<<"x"<<n<<endl;
+  for (int i = n; i --> 0; )
+  {
+    for (int j = n; j --> 0; )
+    {
+      cout<<A[i*n+j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+void print_vector(std::vector<double> x)
+{
+  int n=x.size();
+  cout<<"le vecteur de taille "<<n<<endl;
+  for (int i = 0; i<n; i++)
+  {
+    cout<<x[i]<<" ";
+  }
+  cout<<endl;
+ 
+}
+
 
 
 int main(int argc, char** argv)
@@ -23,7 +53,13 @@ int main(int argc, char** argv)
 
     //chech the algebra
     std::vector<double> A(4),b(2);
+    A[0]=1.;A[1]=2.;A[2]=3.;A[3]=4.;
+    b[0]=5.;b[1]=6.;
     GradConj Gc(A,b);
+
+    //output test
+    print_matrix(A);
+    print_vector(b);
 
   
   // Fin du chrono
